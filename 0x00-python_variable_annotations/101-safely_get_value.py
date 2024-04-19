d313f5337e82ac8ff1832edc9f31c76838f4441a
Seth Ketsi <christambassador12@gmail.com>
2024-04-19 00:17:02 +0000

    12. Type Checking

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Given the parameters and the return values,
-add type annotations to the function
+add type annotations to the function.
 """
 
 
@@ -13,7 +13,7 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
     """Get a value from a mapping safely,
-    returning default if key not present."""
+    returning default if key not present"""
     if key in dct:
         return dct[key]
     else:
