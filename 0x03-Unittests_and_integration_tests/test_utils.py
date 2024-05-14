693c5c7885e92bcdcc93fd36e424d3c9cf012ec0
Seth Ketsi <christambassador12@gmail.com>
2024-05-14 12:13:52 +0000

    write the first unit test for utils.access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Any, Tuple
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,8 +17,21 @@ class TestAccessNestedMap(unittest.TestCase):
         ({'a': {'b': 2}}, ['a', 'b'], 2),
         ({'a': {}}, ['a', 'b'], None),
     ])
-    def test_access_nested_map(self, nested_map: Dict[str, Any], path: Tuple[str], expected: Any):
+    def test_access_nested_map(self, nested_map: Dict[str, Any],
+                               path: Tuple[str], expected: Any) -> None:
         """
         Test access_nested_map Doc
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ['a']),
+        ({'a': 1}, ['a', 'b']),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict[str, Any],
+                                         path: Tuple[str]) -> None:
+        """
+        Test access_nested_map_exception Doc
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
