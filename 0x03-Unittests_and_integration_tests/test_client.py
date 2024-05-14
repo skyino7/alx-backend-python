f27a70fa59b6da1770c23611c9f87884fc1ac18c
Seth Ketsi <christambassador12@gmail.com>
2024-05-14 14:51:55 +0000

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -39,3 +39,16 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("test-org")
         result = client._public_repos_url
         self.assertEqual(result, url)
+
+    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """
+        Test that GithubOrgClient.public_repos returns the correct value
+        """
+        mock_public_repos_url.return_value = "https://api.github.com/orgs/test-org/repos"
+        github_org_client = GithubOrgClient("test-org")
+        result = github_org_client.public_repos()
+        mock_get_json.assert_called_once()
+        mock_public_repos_url.assert_called_once()
+        self.assertEqual(result, ["repo1", "repo2"])
